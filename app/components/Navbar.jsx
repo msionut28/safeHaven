3c1cf37dbd72401d8dbc582335eb32db858c74e5
Stelian-Ionut Molocea <139543968+msionut28@users.noreply.github.com>
2023-11-03 16:09:01 +0200

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -14,12 +14,12 @@ export default function Navbar() {
     <nav className="bg-tomato p-5 flex justify-between items-center shadow-md">
       <Link href="/"><h1 className="text-white cursor-pointer">SafeHaven</h1></Link>
       <div className="flex gap-5">
-        <Link href="/about-us" className="text-white">About Us</Link>
-        <Link href="/MainChat" className="text-white">Chats</Link>
-        <Link href="/events" className="text-white">Events</Link>
-        <Link href="/map" className="text-white">Map</Link>
-        <Link href="/toilets" className="text-white">Toilets</Link>
-        <Link href="/news" className="text-white">News</Link>
+        <Link href="/about-us" className="text-black">About Us</Link>
+        <Link href="/MainChat" className="text-black">Chats</Link>
+        <Link href="/events" className="text-black">Events</Link>
+        <Link href="/map" className="text-black">Map</Link>
+        <Link href="/toilets" className="text-black">Toilets</Link>
+        <Link href="/news" className="text-black">News</Link>
         {session ? <UserProfile user={session.user} /> : null}
         {loggedIn ? userName : <LoginForm />}
       </div>
