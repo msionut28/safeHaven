dcae6160e17818484fcc3083c7941d56efe82557
Stelian-Ionut Molocea <139543968+msionut28@users.noreply.github.com>
2023-11-03 14:18:28 +0200

    Update page.jsx

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState, useEffect } from "react";
-import styles from "./map.module.css";
+//import styles from "./map.module.css";
 import {
   MapContainer,
   TileLayer,
