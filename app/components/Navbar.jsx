8c305e70d899f6b8b5ea4039351ced264f8b8c69
Stelian-Ionut Molocea <139543968+msionut28@users.noreply.github.com>
2023-11-03 15:44:44 +0200

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,42 +1,35 @@
 'use client'
 
-// import { Button } from '@/components/ui/button'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { useSession } from 'next-auth/react'
-import Link from "next/link"
-import LoginForm from './LoginForm/LoginForm'
-import styles from './Navbar.module.css'
-import UserProfile from './UserProfile/UserProfile'
-
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import Link from "next/link";
+import LoginForm from './LoginForm/LoginForm';
+import UserProfile from './UserProfile/UserProfile';
 
 export default function Navbar() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [userName, setUserName] = useState('TESTING ')
-  const router = useRouter()
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState('TESTING');
+  const router = useRouter();
   const { data: session } = useSession();
-  // console.log( session);
 
   function gotoEvents() {
-    router.push('/events')
+    router.push('/events');
   }
 
   return (
-    <nav className={styles.navbar}>
-      <Link href="/"><h1 className={styles.logo}>SafeHaven</h1></Link>
-      <div className={styles.links}>
-        <Link href="/about-us">About Us</Link>
-        <Link href="/MainChat"> Chats</Link>
-        <Link href="/events">Events</Link>
-        <Link href="/map">Map</Link>
-        <Link href="/toilets">Toilets</Link>
-        <Link href="/news">News</Link>
-        {/* <AuthButton onClick={gotoEvents} /> */}
-        {
-          session ? <UserProfile user={session.user}/> : <></>
-        }
-        {loggedIn ? userName : <LoginForm /> }
+    <nav className="bg-tomato p-5 flex justify-between items-center shadow-md">
+      <Link href="/"><h1 className="text-white cursor-pointer">SafeHaven</h1></Link>
+      <div className="flex gap-5">
+        <Link href="/about-us" className="text-white">About Us</Link>
+        <Link href="/MainChat" className="text-white">Chats</Link>
+        <Link href="/events" className="text-white">Events</Link>
+        <Link href="/map" className="text-white">Map</Link>
+        <Link href="/toilets" className="text-white">Toilets</Link>
+        <Link href="/news" className="text-white">News</Link>
+        {session ? <UserProfile user={session.user} /> : null}
+        {loggedIn ? userName : <LoginForm />}
       </div>
     </nav>
-  )
+  );
 }
